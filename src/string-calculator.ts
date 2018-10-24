c6995addc09c16f48c1b943058104f98086ef6c0
Josef Staroba <jstaroba@centene.com>
2018-10-24 11:11:00 -0500

    Made add method static

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,7 +1,7 @@
 import { StringParser } from './string-parser';
 
 export class StringCalculator {
-  add(stringToParse) {
+  static add(stringToParse) {
     let stringParser = new StringParser(stringToParse);
     let arrayToSum = stringParser.parse();
     return arrayToSum.reduce((total, currentValue) => +total + +currentValue);
