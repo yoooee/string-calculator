4bae94984ab7e638aa1b79670c497de968ddfad0
Josef Staroba <jstaroba@centene.com>
2018-10-24 10:53:56 -0500

    Add stringparser tests

---
+++ src/string-parser.spec.ts
@@ -0,0 +1,15 @@
+import { StringParser } from './string-parser';
+
+fdescribe('StringParser', () => {
+  describe('parse', () => {
+    it('should take a string and split it based on custom delimiters', () => {
+      let stringToParse = '1,2,3,4';
+      let stringParser = new StringParser(stringToParse);
+      let subject = stringParser.parse();
+      let expected = ['1', '2', '3', '4'];
+
+      expect(subject.length).toEqual(4);
+      expect(subject).toEqual(expected);
+    });
+  })
+});
