795154f5c103195f6b98944952351b62ae19743a
Josef Staroba <jstaroba@centene.com>
2018-10-27 11:08:02 -0500

    Move negative number check into separate method

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -7,16 +7,20 @@ export class StringCalculator {
   }
 
   static sumAllValues(arrayToSum) {
-    const negativeValues = arrayToSum.filter(currentValue => currentValue < 0);
-
-    if (negativeValues.length > 0) {
-      throw new Error(`negatives not allowed ${negativeValues}`);
-    }
+    StringCalculator.checkForNegativeValues(arrayToSum);
 
     return arrayToSum.reduce((total, currentValue) => {
       if (currentValue > 1000)
         currentValue = 0;
       return parseFloat(total) + parseFloat(currentValue);
     }, 0);
+  }
+
+  static checkForNegativeValues(arrayToCheck) {
+    const negativeValues = arrayToCheck.filter(currentValue => currentValue < 0);
+
+    if (negativeValues.length > 0) {
+      throw new Error(`negatives not allowed ${negativeValues}`);
+    }
   }
 }
